fdf1a57cab39635443e0ecd7fbfdf1376f7e54de
Diki Telyu <dikirahman.me@gmail.com>
2024-04-02 14:14:59 +0700

    Membuat tampilan list

--- app/src/main/java/org/d3if0166/dailytask/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0166/dailytask/ui/screen/MainScreen.kt
@@ -1,8 +1,17 @@
 package org.d3if0166.dailytask.ui.screen
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -10,11 +19,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if0166.dailytask.R
+import org.d3if0166.dailytask.model.Task
 import org.d3if0166.dailytask.ui.theme.DailyTaskTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,12 +56,47 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    Column (
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
 
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data) {
+            ListItem(task = it)
+            Divider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(task: Task) {
+    val checkedState = remember { mutableStateOf(false) }
+    Row (
+        modifier = Modifier.fillMaxWidth().padding(10.dp, 0.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    )
+    {
+        Checkbox(
+            checked = checkedState.value,
+            onCheckedChange = { checkedState.value = it },
+
+        )
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+
+            Text(
+                text = task.judul,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }
 
