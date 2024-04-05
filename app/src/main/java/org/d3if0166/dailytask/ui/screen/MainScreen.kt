f0300178619eebd7ed198236ec7597c86b9d1da1
Diki Telyu <dikirahman.me@gmail.com>
2024-04-05 22:02:49 +0700

    Membuat form buat tugas

--- app/src/main/java/org/d3if0166/dailytask/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0166/dailytask/ui/screen/MainScreen.kt
@@ -5,26 +5,36 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -38,6 +48,13 @@ import org.d3if0166.dailytask.ui.theme.DailyTaskTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
+    val openDialog = remember { mutableStateOf(false) }
+    var namaTugas by remember { mutableStateOf("") }
+    var deskripsiTugas by remember { mutableStateOf("") }
+    var namaTugasError by remember { mutableStateOf(false) }
+    val dateState = rememberDatePickerState()
+
     Scaffold (
     topBar = {
         TopAppBar(
@@ -52,7 +69,9 @@ fun MainScreen() {
     },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = { /*TODO*/ },
+                onClick = {
+                    openDialog.value = true
+                }
             ) {
                 Icon(
                     imageVector = Icons.Filled.Add,
@@ -66,7 +85,85 @@ fun MainScreen() {
 ) { padding ->
     ScreenContent(Modifier.padding(padding))
 }
+
+    if (openDialog.value) {
+            AlertDialog(
+                onDismissRequest = {
+                    openDialog.value = false
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(375.dp)
+                    .padding(16.dp),
+                shape = RoundedCornerShape(16.dp),
+
+                title = {
+                    Text(text = stringResource(id = R.string.buat_tugas))
+                },
+                text = {
+                    Column (
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        OutlinedTextField(
+                            value = namaTugas,
+                            isError = namaTugasError,
+                            supportingText = { ErrorHint(isError = namaTugasError) },
+                            onValueChange = { namaTugas = it },
+                            label = { Text(stringResource(id = R.string.field_nama_tugas)) }
+                        )
+                        OutlinedTextField(
+                            value = deskripsiTugas,
+                            onValueChange = { deskripsiTugas = it },
+                            label = { Text(stringResource(id = R.string.field_deskripsi_tugas)) },
+                            maxLines = 5
+                        )
+//                        Spacer(modifier = Modifier.height(8.dp))
+//                        DatePicker(
+//                            title = {
+//                                Text(
+//                                    text = stringResource(id = R.string.field_tenggat_tugas)
+//                                )
+//                            },
+//                            state = dateState,
+//                            modifier = Modifier.fillMaxWidth(),
+//                        )
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            openDialog.value = false
+                        }
+                    ) {
+                        Text(text = stringResource(id = R.string.tombol_batal))
+                    }
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            namaTugasError = (namaTugas == "")
+                            if (namaTugasError) return@Button
+                            else
+                                openDialog.value = false
+                        }
+                    ) {
+                        Text(text = stringResource(id = R.string.tombol_simpan))
+                    }
+                }
+            )
+        }
+
 }
+
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = stringResource(id = R.string.input_invalid))
+    }
+}
+
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
